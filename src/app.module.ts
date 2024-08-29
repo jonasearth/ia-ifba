2270e96fd9852e9b3031c5d4d58dab5c57d6139c
Jonas Lima <jonasearth1@gmail.com>
2024-08-29 19:57:42 -0300

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { SlrModule } from './slr/slr.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppConfigService } from './app.env.service';
+import { migrations } from './libs/database/migrations';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SlrModule,
+    TypeOrmModule.forRootAsync({
+      name: 'reader',
+      imports: [AppModule],
+      useFactory: (appEnvService: AppConfigService) => ({
+        name: 'reader',
+        type: appEnvService.database.type as any,
+        host: appEnvService.database.hostRead,
+        port: appEnvService.database.port,
+        username: appEnvService.database.username,
+        password: appEnvService.database.password,
+        database: appEnvService.database.database,
+        autoLoadEntities: true,
+        migrations,
+      }),
+      inject: [AppConfigService],
+    }),
+  ],
   controllers: [AppController],
-  providers: [],
+  providers: [AppConfigService],
+  exports: [AppConfigService],
 })
 export class AppModule {}
